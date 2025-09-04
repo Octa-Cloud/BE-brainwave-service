5b0f54257526ff737b633cb1d2013aac29825337
KoungQ <guu119@naver.com>
2025-09-04 15:07:58 +0900

    init

---
+++ src/main/java/com/project/brainwave/BrainwaveServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.brainwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainwaveServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainwaveServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/brainwave/BrainwaveServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.brainwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainwaveServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
